1e8ca0eb31d6a6559904590bda5f18638c971c62
MegaMindCFD <mebiswajithit@gmail.com>
2021-04-03 11:50:36 +0530

    Initial commit: Added stl reader

--- source/cpp/main.cpp
+++ source/cpp/main.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<Mortonhasher.hpp>
-#include<Stl.hpp>
-#include<set>
+#include<Geometry.hpp>
 
 int main()
 {
@@ -16,41 +15,12 @@ int main()
     hash.SetCoord(4,2,0);
     auto ans = hash.Get();
     std::cout << ans << "\n";
-
-    try {
-        stl_reader::StlMesh <float, unsigned int> mesh("C:/Users/MegaMind/Downloads/model/tractor.stl");
-
-        for (size_t itri = 0; itri < mesh.num_tris(); ++itri) {
-            std::cout << "coordinates of triangle " << itri << ": ";
-            for (size_t icorner = 0; icorner < 3; ++icorner) {
-                //float* c = mesh.vrt_coords(mesh.tri_corner_ind(itri, icorner));
-                const float* c = mesh.tri_corner_coords(itri, icorner);
-                // or alternatively:
-                // float* c = mesh.vrt_coords (mesh.tri_corner_ind (itri, icorner));
-                std::cout << "(" << c[0] << ", " << c[1] << ", " << c[2] << ") ";
-            }
-            std::cout << std::endl;
-
-            auto n = mesh.tri_normal(itri);
-            std::cout << "normal of triangle " << itri << ": "
-                << "(" << n[0] << ", " << n[1] << ", " << n[2] << ")\n";
-        }
-    }
-    catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
-    }
     */
 
-    std::set<stl_reader::Coord<double>> crd;
-
-    stl_reader::Coord<double > c,d;
-
-    crd.emplace(c);
-    c.x = 8;
-    crd.emplace(c);
-    crd.emplace(c);
-    crd.emplace(d);
-    std::sort(crd.begin(), crd.end());
+    Geometry geom;
+    geom.Read("C:/Users/MegaMind/Downloads/model/tractor.stl");
+    geom.CalculateBoundingBox();
+    geom.Write("C:/Users/MegaMind/Downloads/model", "tractor");
 
     return 0;
 }
